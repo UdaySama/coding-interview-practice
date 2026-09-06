3ee39afa6ec618b8c1d7e57117faa795323e99a4
UdaySama <udaykalse123@gmail.com>
2026-09-06 22:37:26 +0530

    Practice converting lists to sets and removing duplicates

---
+++ Fundamentals/lesson_01/lesson_27.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 2, 3, 3, 3, 4]
+
+unique_numbers = set(numbers)
+
+print(unique_numbers)
+
+# A. How many values will be in unique_numbers? :- 4 
+
+# B. Will duplicate 2 and 3 remain? :- No
+
+# C. Can you do unique_numbers[0]? Why or why not? :- No we can not do that in set beacuse of unorder 
