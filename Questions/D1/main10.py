c11c73e979557ed7667bdb395bf163c37e424aa0
UdaySama <udaykalse123@gmail.com>
2026-09-18 22:26:49 +0530

    feat: implement palindrome check function

---
+++ Questions/D1/main10.py
@@ -0,0 +1,9 @@
+def is_palindrome(s):
+    cleaned = "".join(char.lower() for char in s if char.isalnum())
+    left, right = 0, len(cleaned) - 1
+    while left < right:
+        if cleaned[left] != cleaned[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
